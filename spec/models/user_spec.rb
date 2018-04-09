81a1cca3abdd3d9161e541597ed3888960c005a7
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-04-09 13:59:07 -0600

    adds user model test to stub oauth

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User, type: :model do
+  it "creates or updates itself from an oauth hash" do
+    auth = {
+      provider: "google",
+      uid: "12355565",
+      info: {
+        email: "test@google.com",
+        first_name: "zac",
+        last_name: "palmquist",
+        image: "www.image.com/image.jpg",
+      },
+      credentials: {
+        oauth_token: "hagshagshsag",
+        refresh_token: "blajw224",
+        expires_at: DateTime.now
+      }
+    }
+    User.update_or_create(auth)
+    new_user = User.first
+
+    expect(new_user.provider).to eq(auth[:provider])
+    expect(new_user.uid).to eq(auth[:uid])
+    expect(new_user.email).to eq(auth[:info][:email])
+    expect(new_user.first_name).to eq(auth[:info][:first_name])
+    expect(new_user.last_name).to eq(auth[:info][:last_name])
+    expect(new_user.image).to eq(auth[:info][:image])
+    expect(new_user.oauth_token).to eq(auth[:credentials][:oauth_token])
+    expect(new_user.oauth_token).to eq(auth[:credentials][:refresh_token])
+    expect(new_user.oauth_token).to eq(auth[:credentials][:expires_at])
+  end
 end
