0ca8f5816c55e6f28586e0711c5c4a80b8f13bdd
kylesallette <kylesallette@gmail.com>
2018-04-09 13:51:27 -0600

    adds user database

---
+++ db/migrate/20180409194836_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :provider
+      t.string :uid
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :image
+      t.string :oauth_token
+      t.string :refresh_token
+      t.datetime :oauth_expires_at
+
+      t.timestamps
+    end
+  end
+end
