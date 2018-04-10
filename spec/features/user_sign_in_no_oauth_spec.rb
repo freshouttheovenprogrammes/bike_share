a783512350daf3851f726b58a94e4848cb80b7eb
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-04-10 10:12:21 -0600

    adds robust feature test for user login

---
+++ spec/features/user_sign_in_no_oauth_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe "as a user" do
+  describe "when I visit the home page" do
+    context "I can sign up for the site without Oauth" do
+      it "and see my profile page" do
+
+      visit "/"
+
+      expect(page).to have_content("Sign Up For An Account!")
+      click_link("Sign Up For An Account!")
+
+      expect(current_path).to eq "users/new"
+
+      fill_in("user[username]").with "Testyguy"
+      fill_in("user[email]").with "test@gmail.com"
+      fill_in("user[password]").with "test123"
+      click_link("Register")
+
+      new_user = User.last
+      expect(current_path).to eq user_show_path(new_user)
+      expect(page).to have_content("Welcome to your profile, #{new_user.username}!")
+      end
+    end
+  end
+end
