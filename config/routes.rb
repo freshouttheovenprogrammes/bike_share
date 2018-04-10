1fc05dbfdc4518249ec1582baebaddc11819649f
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-04-10 16:24:29 -0600

    adds link for regular sign in, need to work out google oauth + regular user stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
+  get 'login', to: 'sessions#create', as: 'signin'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
   root to: "home#index"
