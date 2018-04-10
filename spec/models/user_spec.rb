722e8cb66559bb5530b20e02d37d55ea54f85594
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-04-10 13:56:32 -0600

    adds some more tests to user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,6 +26,16 @@ describe User, type: :model do
     expect(new_user.last_name).to eq(auth[:info][:last_name])
     expect(new_user.image).to eq(auth[:info][:image])
     expect(new_user.oauth_token).to eq(auth[:credentials][:token])
-    expect(new_user.oauth_expires_at).to eq(auth[:credentials][:expires_at])
+  end
+  it "regular user can be created" do
+    user = User.create!(username: "Testname", first_name: "Bob", last_name: "Still bob", password: "badpassword", email: "email@email.com")
+
+    expect(user).to be_valid
+  end
+  it "uniqueness of username regardless of case_use" do
+    User.create(username: "TEsTnAmE", first_name: "Bob", last_name: "Still bob", password: "badpassword", email: "email@email.com")
+    user2 = User.create(username: "testname", first_name: "2222Bob", last_name: "2222Still bob", password: "badpassword", email: "23232email@email.com")
+
+    expect(user2).to_not be_valid
   end
 end
