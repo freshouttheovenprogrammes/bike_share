97943c0b213e40b1aea9212a9e0141898a6c847b
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-04-13 13:21:56 -0600

    fix typo

--- app/validators/credential_validator.rb
+++ app/validators/credential_validator.rb
@@ -1,7 +1,7 @@
 class CredentialValidator < ActiveModel::Validator
 
   def validate(record)
-    if (record.app_credential.nil? || recoard.app_credential.password_digest.nil?) && (record.google_credential.nil? || record.google_credential.uid.nil?)
+    if (record.app_credential.nil? || record.app_credential.password_digest.nil?) && (record.google_credential.nil? || record.google_credential.uid.nil?)
       record.errors[:no_credential_error] << "user must have credentials"
     end
   end
