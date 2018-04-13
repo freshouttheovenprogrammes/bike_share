9271df4e0b045bfeb725b1e5eea5cb527812402e
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-04-13 11:29:27 -0600

    updates user method to work with new tables

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,30 @@
 class User < ApplicationRecord
-  has_secure_password validations: false
   validates :username, uniqueness: { case_sensitive: false }
-  validates :role, presence: true
+  validates_presence_of :email, :first_name, :last_name
+  include ActiveModel::Validations
+  validates_with CredentialValidator
+  has_one :app_credential, dependent: :destroy
+  has_one :googlecredential, dependant: :destroy
 
   enum role: %w(default admin)
-  
+
   def self.update_or_create(auth)
-    where(provider: auth[:provider], uid: auth[:uid]).first_or_initialize.tap do |user|
-      user.provider = auth[:provider]
-      user.uid = auth[:uid]
-      user.first_name = auth[:info][:first_name]
-      user.last_name = auth[:info][:last_name]
-      user.email = auth[:info][:email]
-      user.image = auth[:info][:image]
-      user.oauth_token = auth[:credentials][:token]
-      user.oauth_expires_at = Time.at(auth[:credentials][:expires_at])
+    google_cred = GoogleCredential.find_by(uid: auth[:uid]) || GoogleCredential.new
+    google_cred.attributes = {
+      provider: auth[:provider],
+      uid: auth[:uid],
+      oauth_token: auth[:credentials][:token],
+      refresh_token: auth[:credentials][:refresh_token],
+      oauth_expires_at: auth[:credentials][:expires_at]
+      }
+    user = google_cred.update_or_create({
+      email: auth[:info][:email],
+      first_name: auth[:info][:first_name],
+      last_name: auth[:info][:last_name],
+      image_url: auth[:info][:image]
+      })
       user.save!
-    end
+      google_cred.save!
+      user
   end
 end
