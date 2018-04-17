39ddce452dd08320adedc4de6e02ad629cbcf989
kylesallette <kylesallette@gmail.com>
2018-04-17 16:34:17 -0600

    adding stations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180413164952) do
+ActiveRecord::Schema.define(version: 20180417222808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,17 @@ ActiveRecord::Schema.define(version: 20180413164952) do
   create_table "orders", force: :cascade do |t|
     t.integer "total"
     t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "stations", force: :cascade do |t|
+    t.string "name"
+    t.float "latitude"
+    t.float "longitude"
+    t.integer "dock_count"
+    t.string "city"
+    t.datetime "installation_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
