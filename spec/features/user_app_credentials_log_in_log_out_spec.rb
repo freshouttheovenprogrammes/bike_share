d94d1edc2d174cae1bbe4d96a4f260a8f34928af
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-04-18 13:20:37 -0600

    more login/logout testing

--- spec/features/user_app_credentials_log_in_log_out_spec.rb
+++ spec/features/user_app_credentials_log_in_log_out_spec.rb
@@ -7,6 +7,7 @@ describe "As a user" do
     visit "/"
 
     click_link("Log In")
+
     expect(current_path).to eq signin_path
 
     fill_in "username", with: app_user.username
@@ -15,5 +16,21 @@ describe "As a user" do
 
     expect(current_path).to eq users_path(app_user)
     expect(page).to have_content "Welcome to your profile, #{app_user.first_name}!"
+  end
+
+  it "I can log out after signing in" do
+    visit "/"
+
+    click_link("Log In")
+
+    expect(current_path).to eq signin_path
+
+    fill_in "username", with: app_user.username
+    fill_in "password", with: app_user.app_credential.password
+    click_on("Submit")
+
+    click_link("Sign Out")
+
+    expect(current_path). to eq root_path
   end
 end
