0a7748fca58cd20abeb1db6d98868c84da98ce11
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-04-25 10:23:10 -0600

    now tests adding and deleting item from cart

--- spec/features/cart/visitor_should_always_see_cart_information_spec.rb
+++ spec/features/cart/visitor_should_always_see_cart_information_spec.rb
@@ -61,6 +61,40 @@ describe "As a visitor" do
           expect(page).to have_content "Cart: 0"
         end
       end
+      context "and add an item to the cart" do
+        it "the cart amount increases" do
+          category = Category.create!(id: 4, title: "lessons")
+          Item.create!(title: "accessory", description: "hey", price: 300, image: 'mountain_bike.png', status: "active",
+                      quantity: 2, category: category)
+
+          visit lessons_path
+
+          click_button "Add Item To Cart"
+
+          within("#nav-container") do
+            expect(page).to have_content "Cart: 1"
+          end
+        end
+      end
+      context "and delete an item from the cart" do
+        it "the cart amount decreases" do
+          category = Category.create!(id: 4, title: "lessons")
+          Item.create!(title: "accessory", description: "hey", price: 300, image: 'mountain_bike.png', status: "active",
+                      quantity: 2, category: category)
+
+          visit lessons_path
+
+          click_button "Add Item To Cart"
+
+          visit cart_index_path
+
+          click_button "Delete Item From Cart"
+
+          within("#nav-container") do
+            expect(page).to have_content "Cart: 0"
+          end
+        end
+      end
     end
   end
 end
