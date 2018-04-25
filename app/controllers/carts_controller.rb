5e4da903c9c7602ebe4db34cdafe8c7fde7b52c6
kylesallette <kylesallette@gmail.com>
2018-04-25 10:57:32 -0600

    fixed nav overlay

---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,14 @@
+class CartsController < ApplicationController
+  include ActionView::Helpers::TextHelper
+
+  def create
+    @item = Item.find(params[:item_id])
+
+    @cart = Cart.new(session[:cart])
+    @cart.add_item(@item.id)
+    session[:cart] = @cart.contents
+
+
+    redirect_to items_path, notice:"You now have #{pluralize(@cart.count_of(@item.id), @item.title)} in cart."
+  end
+end
