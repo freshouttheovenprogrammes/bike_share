f529a50d5e5e57b3ce1fe0eb7bc2d4c783be71d0
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-04-25 10:26:49 -0600

    price showing on cart path

--- spec/features/cart/visitor_should_always_see_cart_information_spec.rb
+++ spec/features/cart/visitor_should_always_see_cart_information_spec.rb
@@ -75,6 +75,19 @@ describe "As a visitor" do
             expect(page).to have_content "Cart: 1"
           end
         end
+        it "the cart total amount increases" do
+          category = Category.create!(id: 4, title: "lessons")
+          Item.create!(title: "accessory", description: "hey", price: 300, image: 'mountain_bike.png', status: "active",
+                      quantity: 2, category: category)
+
+          visit lessons_path
+
+          click_button "Add Item To Cart"
+
+          visit cart_index_path
+
+          expect(page).to have_content("$300.00")
+        end
       end
       context "and delete an item from the cart" do
         it "the cart amount decreases" do
