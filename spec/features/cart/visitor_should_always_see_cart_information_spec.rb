ad60cfc8f69edbc774d68b8f1d6e3084966c47ac
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-04-25 10:11:58 -0600

    robust testing for cart display information

--- spec/features/cart/visitor_should_always_see_cart_information_spec.rb
+++ spec/features/cart/visitor_should_always_see_cart_information_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "As a visitor" do
   context "when I visit any page" do
-    context "I should see a button for 'Add to Cart' in the nav " do
+    context "I should see a button for 'Add to Cart' in the nav" do
       it "on the bikes path" do
       Category.create!(title: "bikes")
       Item.create!(title: "bike", description: "hey", price: 300, image: 'mountain_bike.png', status: "active",
@@ -14,6 +14,52 @@ describe "As a visitor" do
       end
 
       expect(page).to have_button "Add Item To Cart"
+      end
+      it "on the accessories path" do
+        category = Category.create!(id: 2, title: "accessories")
+        Item.create!(title: "accessory", description: "hey", price: 300, image: 'mountain_bike.png', status: "active",
+                    quantity: 2, category: category)
+
+        visit accessories_path
+
+        within("#nav-container") do
+          expect(page).to have_content "Cart: 0"
+        end
+
+        expect(page).to have_button "Add Item To Cart"
+      end
+      it "on the tools path" do
+        category = Category.create!(id: 3, title: "tools")
+        Item.create!(title: "accessory", description: "hey", price: 300, image: 'mountain_bike.png', status: "active",
+                    quantity: 2, category: category)
+
+        visit tools_path
+
+        within("#nav-container") do
+          expect(page).to have_content "Cart: 0"
+        end
+
+        expect(page).to have_button "Add Item To Cart"
+      end
+      it "on the lessons path" do
+        category = Category.create!(id: 4, title: "lessons")
+        Item.create!(title: "accessory", description: "hey", price: 300, image: 'mountain_bike.png', status: "active",
+                    quantity: 2, category: category)
+
+        visit lessons_path
+
+        within("#nav-container") do
+          expect(page).to have_content "Cart: 0"
+        end
+
+        expect(page).to have_button "Add Item To Cart"
+      end
+      it "on the dashboard" do
+
+        visit root_path
+        within("#nav-container") do
+          expect(page).to have_content "Cart: 0"
+        end
       end
     end
   end
