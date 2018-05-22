c14c3f060fc4d00aec6b2529564698d8c0024382
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-05-21 19:49:11 -0600

    build out test that admin can't change user information

---
+++ spec/features/admin/admin_cant_change_user_information_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+context "as an admin" do
+  let(:user) { FactoryBot.create(:app_user) }
+  let(:admin) { FactoryBot.create(:admin_app) }
+  before(:each) do
+    visit signin_path
+
+    fill_in "username", with: admin.username
+    fill_in "password", with: admin.app_credential.password
+    click_on "Submit"
+  end
+  it "i cannot change another users profile" do
+    visit edit_user_path(user)
+
+    page.status_code.should be 404
+  end
+end
