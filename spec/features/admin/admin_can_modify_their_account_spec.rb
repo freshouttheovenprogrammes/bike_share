a523bff0c1ec72b3e3997f7d5f0b4881a3ae6822
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-05-21 19:41:56 -0600

    build out admin edit test

---
+++ spec/features/admin/admin_can_modify_their_account_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+context "as an admin" do
+  let(:admin) { FactoryBot.create(:admin_app) }
+  
+  before(:each) do
+    visit signin_path
+
+    fill_in "username", with: admin.username
+    fill_in "password", with: admin.app_credential.password
+    click_on "Submit"
+  end
+  it "i can edit my own account" do
+    click_link "Edit Account Details"
+
+    fill_in "user[first_name]", with: "changed"
+    fill_in "user[last_name]", with: "new name"
+    fill_in "user[email]", with: "new.email@gmail.com"
+    fill_in "user[username]", with: "pineapple-pen"
+    fill_in "user[password]", with: "crazyasspassword"
+
+    click_on "Update Profile"
+
+    expect(current_path).to eq dashboard_path(app_user)
+    expect(page).to have_content "changed"
+    expect(page).to have_content "new name"
+    expect(page).to have_content "new.email@gmail.com"
+    expect(page).to have_content "pineapple-pen"
+  end
+end
